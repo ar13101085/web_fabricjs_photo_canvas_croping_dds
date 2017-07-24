c024c6d722c8e05d8aaaeb90bee38c7fa2f5523d
ar13101085 <ar13101085@gmail.com>
2017-07-24 19:04:45 +0600

    Signed-off-by: ar13101085 <ar13101085@gmail.com>

---
+++ test.js
@@ -0,0 +1,114 @@
+{
+    "vocperson_test07_000377": {
+    "anno": {
+        "bbox": [
+            {
+                "box": [115.0, 73.0, 86.0, 155.0],
+                "status": "standing",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [181.0, 90.0, 89.0, 131.0],
+                "status": "standing",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [203.0, 149.0, 112.0, 114.0],
+                "status": "other",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [90.0, 158.0, 120.0, 108.0],
+                "status": "standing",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [22.0, 80.0, 99.0, 175.0],
+                "status": "other",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [386.0, 77.0, 107.0, 176.0],
+                "status": "standing",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [339.0, 89.0, 67.0, 205.0],
+                "status": "other",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }, {
+                "box": [284.0, 99.0, 71.0, 171.0],
+                "status": "standing",
+                "keypoints": [{
+                    "keyname": "kename_1",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_2",
+                    "keypoint": [100, 200]
+                }, {
+                    "keyname": "kename_n",
+                    "keypoint": [100, 200]
+                }]
+            }],
+            "size": [500.0, 375.0]
+    },
+    "imgpath": "vocperson_test07_000377.jpg"
+}
+}
